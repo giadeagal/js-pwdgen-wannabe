cd4be465d44c46e1b93426aaca3a1a3604a08988
Gianluca <gl.galassini@gmail.com>
2021-05-27 00:08:10 +0200

    minuscole

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-var UserName;
-UserName = prompt ("Benvenuti nel mio generatore di password SUPER-inefficaci. Per iniziare mi servirebbe il tuo nome di battesimo");
+var userName;
+userName = prompt ("Benvenuti nel mio generatore di password SUPER-inefficaci. Per iniziare mi servirebbe il tuo nome di battesimo");
 
-var UserSurname;
-UserSurname = prompt ("Perfetto, ora però  mi servirebbe anche il cognome, se non ti dispiace :D");
+var userSurname;
+userSurname = prompt ("Perfetto, ora però  mi servirebbe anche il cognome, se non ti dispiace :D");
 
-var FavColor;
-FavColor = prompt ("Abbiamo quasi finito! Ora mi serve solo sapere il tuo colore preferito.");
+var favColor;
+favColor = prompt ("Abbiamo quasi finito! Ora mi serve solo sapere il tuo colore preferito.");
 
-document.getElementById("stampante").innerHTML="Ecco la tua password personalizzata: " + UserName + UserSurname + FavColor + 21;
+document.getElementById("stampante").innerHTML="Ecco la tua password personalizzata: " + userName + userSurname + favColor + 21;

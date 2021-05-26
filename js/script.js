5e6f67a69a009bb113593cf8a1f02a99181a85cb
Gianluca <gl.galassini@gmail.com>
2021-05-26 18:09:36 +0200

    aggiustamenti

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-console.log("nomecognomecolorepreferito21")
-
 var UserName;
 UserName = prompt ("Benvenuti nel mio generatore di password SUPER-inefficaci. Per iniziare mi servirebbe il tuo nome di battesimo");
 
 var UserSurname;
-UserSurname = prompt ("Perfetto, ora però  mi servirebbe anche il cognome, se non ti dispiace :D")
+UserSurname = prompt ("Perfetto, ora però  mi servirebbe anche il cognome, se non ti dispiace :D");
 
 var FavColor;
-FavColor = prompt ("Abbiamo quasi finito! Ora mi serve solo sapere il tuo colore preferito.")
+FavColor = prompt ("Abbiamo quasi finito! Ora mi serve solo sapere il tuo colore preferito.");
 
-alert ("Ecco la tua password personalizzata:\n" + "\n" + UserName + UserSurname + FavColor + "21" + "\n" +"\nIo la trovo bellissima, ma ricordati di non usarla per dati importanti!")
+document.getElementById("stampante").innerHTML="Ecco la tua password personalizzata: " + UserName + UserSurname + FavColor + 21;

bf69be37e9a8427863a3986a7b6e5dc64d97f3e2
Gianluca <gl.galassini@gmail.com>
2021-05-26 17:38:41 +0200

    fine versione base

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 console.log("nomecognomecolorepreferito21")
 
 var UserName;
+UserName = prompt ("Benvenuti nel mio generatore di password SUPER-inefficaci. Per iniziare mi servirebbe il tuo nome di battesimo");
 
 var UserSurname;
+UserSurname = prompt ("Perfetto, ora però  mi servirebbe anche il cognome, se non ti dispiace :D")
 
 var FavColor;
+FavColor = prompt ("Abbiamo quasi finito! Ora mi serve solo sapere il tuo colore preferito.")
+
+alert ("Ecco la tua password personalizzata:\n" + "\n" + UserName + UserSurname + FavColor + "21" + "\n" +"\nIo la trovo bellissima, ma ricordati di non usarla per dati importanti!")
